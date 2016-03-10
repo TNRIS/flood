cc1108eae7a3db3ab803180dfad3b07f33801662
James Seppi <james.seppi@gmail.com>
2016-03-10 15:11:25 -0600

    cleanup for eslint

--- web-viewer/src/entry.jsx
+++ web-viewer/src/entry.jsx
@@ -1,24 +1,24 @@
 
-import React from 'react';
-import {render} from 'react-dom';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-import App from './components/App';
-import rootReducer from './reducers';
+import App from './components/App'
+import rootReducer from './reducers'
 
-import './sass/main.scss';
+import './sass/main.scss'
 
 // vendor css and js
-import './vendor/material.css';
-import './vendor/material.js';
+import './vendor/material.css'
+import './vendor/material.js'
 
 const store = (window.devToolsExtension ?
   window.devToolsExtension()(createStore)
-  : createStore)(rootReducer);
+  : createStore)(rootReducer)
 
 render((
   <Provider store={store}>
     <App />
   </Provider>
-), document.getElementById('reactApp'));
+), document.getElementById('reactApp'))
