57797d3a94602139cdcb12b178542fce56ddbaba
James Seppi <james.seppi@gmail.com>
2016-03-10 14:08:12 -0600

    add dropdown icon

--- web-viewer/src/components/NavigationBar.jsx
+++ web-viewer/src/components/NavigationBar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Navigation } from 'react-mdl'
+import { Navigation, Icon } from 'react-mdl'
 
 import BaseLayerMenuContainer from '../containers/BaseLayerMenuContainer'
 
 const NavigationBar = () => (
   <Navigation>
-    <a href="" onClick={(e) => e.preventDefault()} id="baselayer-menu">Basemap</a>
+    <a href="" onClick={(e) => e.preventDefault()} id="baselayer-menu">
+      Basemap <Icon name="arrow_drop_down" />
+    </a>
     <BaseLayerMenuContainer target="baselayer-menu" />
     <a href="">Flood Preparation</a>
     <a href="">About this Site</a>
