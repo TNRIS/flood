e6c203f94db21bec37b459173c7b00fa92e22408
James Seppi <james.seppi@gmail.com>
2016-03-10 13:20:28 -0600

    cln: remove unnecessary function cruft

--- web-viewer/src/components/NavigationBar.jsx
+++ web-viewer/src/components/NavigationBar.jsx
@@ -3,15 +3,13 @@ import { Navigation } from 'react-mdl'
 
 import BaseLayerMenuContainer from '../containers/BaseLayerMenuContainer'
 
-const NavigationBar = () => {
-  return (
-    <Navigation>
-      <a href="" onClick={(e) => e.preventDefault()} id="baselayer-menu">Basemap</a>
-      <BaseLayerMenuContainer target="baselayer-menu" />
-      <a href="">Flood Preparation</a>
-      <a href="">About this Site</a>
-    </Navigation>
-  )
-}
+const NavigationBar = () => (
+  <Navigation>
+    <a href="" onClick={(e) => e.preventDefault()} id="baselayer-menu">Basemap</a>
+    <BaseLayerMenuContainer target="baselayer-menu" />
+    <a href="">Flood Preparation</a>
+    <a href="">About this Site</a>
+  </Navigation>
+)
 
 export default NavigationBar
