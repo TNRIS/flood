757a112042c2368f147053cb014dbcab439b87be
wilsaj <wilson.andrew.j+github@gmail.com>
2016-03-15 16:21:45 -0500

    switch layerCache over to baseLayer API w/separate type and options

--- web-viewer/src/components/Map.jsx
+++ web-viewer/src/components/Map.jsx
@@ -147,7 +147,7 @@ const Map = React.createClass({
   initializeLayerCache(props) {
     const layerCache = this.layerCache
     props.featureLayers.layers.forEach((layer) => {
-      layerCache.add(layer.id, layer.layerInfo)
+      layerCache.add(layer.id, layer.type, layer.options)
     })
   },
   render() {
