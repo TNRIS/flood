ff4a90e886fb36f908b7358e5e6f2cbe40f7c871
wilsaj <wilson.andrew.j+github@gmail.com>
2016-03-29 15:56:45 -0500

    fix flood gauge sql statement - some small syntax errors - order was backwards

--- src/util/cartodb/nws-ahps-gauges-texas.sql
+++ src/util/cartodb/nws-ahps-gauges-texas.sql
@@ -1,4 +1,4 @@
-SELECT * FROM tnris.nws_ahps_gauges_texas
+SELECT * FROM nws_ahps_gauges_texas_copy
   LEFT JOIN
     (VALUES
       ('cc33ff', 1),
@@ -9,7 +9,7 @@ SELECT * FROM tnris.nws_ahps_gauges_texas
       ('72afe9', 6),
       ('906320', 7),
       ('bdc2bb', 8),
-      ('666666', 9),
-    ) as render_order(obs_status, render_z)
-  ON render_order.obs_status = tnris.nws_ahps_gauges_texas.obs_status
-ORDER BY render_z
+      ('666666', 9)
+    ) as render_order(r_obs_status, render_z)
+  ON render_order.r_obs_status = nws_ahps_gauges_texas_copy.obs_status
+ORDER BY render_z desc
