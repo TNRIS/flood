4137377041577b755ce25a101119c8eb1abee878
wilsaj <wilson.andrew.j+github@gmail.com>
2016-04-22 22:09:50 -0500

    simplify image sizing (just use CSS instead of JS-based solution) is more performant and avoids some rare sizing glitches

--- src/components/PopupImage.jsx
+++ src/components/PopupImage.jsx
@@ -1,53 +1,17 @@
 import React, { Component, PropTypes } from 'react'
 
-//export class PopupImage extends Component {
-  //constructor(props) {
-    //super(props)
-  //}
-
-  //render() {
-    //const { src, updatePopup, popupWidth } = this.props
-
-    //const imgStyle = {
-      //maxWidth: (popupWidth - 9.5) + 'px !important'
-    //}
-
-    //return (
-      //<div className="info__image">
-        //hi
-        //<img ref="image" src={src} style={imgStyle} onLoad={() => {updatePopup()}}  />
-      //</div>
-    //)
-  //}
-//}
-
 export default class PopupImage extends Component {
   static propTypes = {
     src: PropTypes.string,
     popupWidth: PropTypes.number,
-    updatePopup: PropTypes.func,
-  }
-
-  componentDidUpdate() {
-    // need to manually set graph image style property
-    //   see: https://github.com/facebook/react/issues/1881
-    // TLDR; !important is not important to React devs
-    const width = this.props.popupWidth - 9.5 - 9.5 - 9.5 - 9.5
-
-    this.refs.image.style.setProperty('max-width', `${width}px`, 'important')
-    this.props.updatePopup()
   }
 
   render() {
-    const { src, updatePopup, popupWidth } = this.props
-
-    const imgStyle = {
-      maxWidth: (popupWidth - 9.5) + 'px !important'
-    }
+    const { src, updatePopup } = this.props
 
     return (
       <div className="info__image">
-        <img ref="image" src={src} style={imgStyle} onLoad={() => {updatePopup()}}  />
+        <img ref="image" src={src} onLoad={() => {updatePopup()}}  />
       </div>
     )
   }
