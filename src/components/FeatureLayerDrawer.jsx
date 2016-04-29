8b1d21892975e8193ab302f5912d9f7923e61e7c
wilsaj <wilson.andrew.j+github@gmail.com>
2016-04-29 11:14:49 -0500

    change title for USGS dashboard

--- src/components/FeatureLayerDrawer.jsx
+++ src/components/FeatureLayerDrawer.jsx
@@ -41,7 +41,7 @@ const FeatureLayerDrawer = ({ layers, onLayerClick }) => {
         <Navigation className="resources__links">
           <ResourceLink text="Preparing for a Flood" href="http://www.twdb.texas.gov/flood/flood-prep-before.asp" />
           <ResourceLink text="What to do during a Flood" href="http://www.twdb.texas.gov/flood/flood-prep-during.asp" />
-          <ResourceLink text="USGS Flood Dashboard" href="http://txpub.usgs.gov/txwaterdashboard/" />
+          <ResourceLink text="USGS Water Dashboard" href="http://txpub.usgs.gov/txwaterdashboard/" />
         </Navigation>
       </div>
 
