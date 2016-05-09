55a9650d3a833eedd7a088e6498d8ff4500abb71
wilsaj <wilson.andrew.j+github@gmail.com>
2016-05-09 17:18:36 -0500

    try switching to MDL footer to fix safari scroll issue

--- src/components/FeatureLayerDrawer.jsx
+++ src/components/FeatureLayerDrawer.jsx
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react'
-import { Drawer, Navigation } from 'react-mdl'
+import { Content, Drawer, Layout, Footer, Navigation } from 'react-mdl'
 
 import FeatureLayer from './FeatureLayer'
 import ResourceLink from './ResourceLink'
@@ -10,54 +10,58 @@ import TexasFloodLogoImage from '../images/texas_flood_logo_transparent.png'
 const FeatureLayerDrawer = ({ layers, onLayerClick }) => {
   return (
     <Drawer className="nav">
-      <div className="nav__head">
-        <div className="nav__title">
-          <a href="http://texasflood.org">
-            <img src={TexasFloodLogoImage} alt="The Texas Flood dot org logo" />
-          </a>
-        </div>
-        <div className="nav__subtitle">
-          Tools for Texans to track flood conditions in their area, in real-time.
-        </div>
-      </div>
-
-      <Navigation className="nav__layers">
-        {layers.map(layer =>
-          <FeatureLayer
-            key={layer.id}
-            icon={layer.icon}
-            active={layer.active}
-            status={layer.status}
-            onClick={() => onLayerClick(layer.id)}
-            {...layer}
-          />
-        )}
-      </Navigation>
-
-      <div className="resources">
-        <div className="resources__title">
-          <div className="resources__title-text">
-            More Resources
+      <Layout>
+        <Content>
+          <div className="nav__head">
+            <div className="nav__title">
+              <a href="http://texasflood.org">
+                <img src={TexasFloodLogoImage} alt="The Texas Flood dot org logo" />
+              </a>
+            </div>
+            <div className="nav__subtitle">
+              Tools for Texans to track flood conditions in their area, in real-time.
+            </div>
           </div>
-        </div>
 
-        <Navigation className="resources__links">
-          <ResourceLink text="Preparing Before Flood" href="http://www.twdb.texas.gov/flood/prep/before.asp" />
-          <ResourceLink text="Being Safe During Flood" href="http://www.twdb.texas.gov/flood/prep/during.asp" />
-          <ResourceLink text="Recovering After Flood" href="http://www.twdb.texas.gov/flood/prep/after.asp" />
-        </Navigation>
-      </div>
+          <Navigation className="nav__layers">
+            {layers.map(layer =>
+              <FeatureLayer
+                key={layer.id}
+                icon={layer.icon}
+                active={layer.active}
+                status={layer.status}
+                onClick={() => onLayerClick(layer.id)}
+                {...layer}
+              />
+            )}
+          </Navigation>
 
-      <div className="footer">
-        <div className="footer__wrapper">
-          <div className="footer__made-by">
-            Made by the
+          <div className="resources">
+            <div className="resources__title">
+              <div className="resources__title-text">
+                More Resources
+              </div>
+            </div>
+
+            <Navigation className="resources__links">
+              <ResourceLink text="Preparing Before Flood" href="http://www.twdb.texas.gov/flood/prep/before.asp" />
+              <ResourceLink text="Being Safe During Flood" href="http://www.twdb.texas.gov/flood/prep/during.asp" />
+              <ResourceLink text="Recovering After Flood" href="http://www.twdb.texas.gov/flood/prep/after.asp" />
+            </Navigation>
           </div>
-          <a className="footer__twdb-logo" href="http://www.twdb.texas.gov">
-            <img src={TWDBLogoImage} alt="The Texas Water Development Board logo" />
-          </a>
-        </div>
-      </div>
+        </Content>
+
+        <Footer>
+          <div className="footer__wrapper">
+            <div className="footer__made-by">
+              Made by the
+            </div>
+            <a className="footer__twdb-logo" href="http://www.twdb.texas.gov">
+              <img src={TWDBLogoImage} alt="The Texas Water Development Board logo" />
+            </a>
+          </div>
+        </Footer>
+      </Layout>
     </Drawer>
   )
 }
