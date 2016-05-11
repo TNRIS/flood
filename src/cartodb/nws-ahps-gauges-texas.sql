a679159f698bdca6fc984ba26f4cb77f2903426e
wilsaj <wilson.andrew.j+github@gmail.com>
2016-05-11 11:17:15 -0500

    put the out of service and out of date gauges at the bottom of the render stack

--- src/cartodb/nws-ahps-gauges-texas.sql
+++ src/cartodb/nws-ahps-gauges-texas.sql
@@ -7,7 +7,8 @@ SELECT * FROM nws_ahps_gauges_texas
       ('action', 60),
       ('no flooding', 50),
       ('not defined', 40),
-      ('low', 30)
+      ('low', 30),
+      ('', 20)
     ) as render_order(r_sigstage, render_z)
-  ON render_order.r_sigstage = nws_ahps_gauges_texas.sigstage
+  ON render_order.r_sigstage = concat('', nws_ahps_gauges_texas.sigstage)
 ORDER BY render_z asc
