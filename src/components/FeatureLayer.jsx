148f2b3d0c5691781f3e8c1f84b7802006650b18
wilsaj <wilson.andrew.j+github@gmail.com>
2016-05-18 17:52:57 -0500

    refactor FeatureLayer into class style component for extendability, etc

--- src/components/FeatureLayer.jsx
+++ src/components/FeatureLayer.jsx
@@ -1,32 +1,43 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 import { Checkbox, Spinner } from 'react-mdl'
 
-const FeatureLayer = ({ onClick, text, icon, active, status }) => {
-  let statusIndicator
-  if (active && status !== 'ready') {
-    statusIndicator = <Spinner />
-  }
-  else {
-    statusIndicator = <Checkbox checked={active} readOnly />
+
+export default class FeatureLayer extends Component {
+  static propTypes = {
+    active: PropTypes.bool,
+    icon: PropTypes.string,
+    onClick: PropTypes.func,
+    status: PropTypes.string,
+    text: PropTypes.string,
   }
 
-  return (
-    <a onClick={(e) => {e.preventDefault(); onClick()}} className="feature-layer__link mdl-navigation__link" href="">
-      <div className="feature-layer__wrapper">
-        <div className="feature-layer__icon-wrapper">
-          <img src={icon} className="feature-layer__icon" />
-        </div>
-        <div className="feature-layer__name vertically-centered__wrapper">
-          <div className="vertically-centered__element">
-            { text }
+  render() {
+    const { onClick, text, icon, active, status } = this.props
+
+    let statusIndicator
+    if (active && status !== 'ready') {
+      statusIndicator = <Spinner />
+    }
+    else {
+      statusIndicator = <Checkbox checked={active} readOnly />
+    }
+
+    return (
+      <a onClick={(e) => {e.preventDefault(); onClick()}} className="feature-layer__link mdl-navigation__link" href="">
+        <div className="feature-layer__wrapper">
+          <div className="feature-layer__icon-wrapper">
+            <img src={icon} className="feature-layer__icon" />
+          </div>
+          <div className="feature-layer__name vertically-centered__wrapper">
+            <div className="vertically-centered__element">
+              { text }
+            </div>
+          </div>
+          <div className="feature-layer__checkbox">
+            { statusIndicator }
           </div>
         </div>
-        <div className="feature-layer__checkbox">
-          { statusIndicator }
-        </div>
-      </div>
-    </a>
-  )
+      </a>
+    )
+  }
 }
-
-export default FeatureLayer
