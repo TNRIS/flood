62410a338ab8700e505ef74e9714e8603df572ed
wilsaj <wilson.andrew.j+github@gmail.com>
2016-05-20 07:24:36 -0500

    rm warning banner

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,11 +10,6 @@ import { colors } from '../constants'
 const App = () => (
   <div>
     <Layout fixedHeader fixedDrawer style={{background: 'white'}}>
-      <Header style={{backgroundColor: colors.twdbBlue}}>
-        <HeaderRow className="site-warning">
-          <strong>Warning:</strong> This site is currently in development and should not be relied upon for any real information.
-        </HeaderRow>
-      </Header>
       <FeatureLayerChooser />
       <Content>
         <MapContainer />
