bc0613dcb6b0b5b8a90db941167a4ccc5a2c623b
David Hickman <davidhickman13@gmail.com>
2017-03-30 12:05:13 -0500

    adding Contact Us link in more resources

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -5,14 +5,19 @@ import ResourceLink from './ResourceLink'
 import AboutLinkContainer from '../containers/AboutLinkContainer'
 
 const Resources = () => {
+  const contactLink = "mailto:tnrisdatasupport@twdb.texas.gov?subject=TexasFlood.org Version: " + VERSION
   return (
       <div className="resources">
         <h4 className="resources__header">More Resources</h4>
         <Navigation className="resources__links">
-          <ResourceLink text="Preparing Before Flood" href="http://www.twdb.texas.gov/flood/prep/before.asp" target="before"/>
-          <ResourceLink text="Being Safe During Flood" href="http://www.twdb.texas.gov/flood/prep/during.asp" target="during"/>
-          <ResourceLink text="Recovering After Flood" href="http://www.twdb.texas.gov/flood/prep/after.asp" target="after"/>
+          <ResourceLink text="Preparing Before Flood"
+                        href="http://www.twdb.texas.gov/flood/prep/before.asp" target="before"/>
+          <ResourceLink text="Being Safe During Flood"
+                        href="http://www.twdb.texas.gov/flood/prep/during.asp" target="during"/>
+          <ResourceLink text="Recovering After Flood"
+                        href="http://www.twdb.texas.gov/flood/prep/after.asp" target="after"/>
           <AboutLinkContainer />
+          <ResourceLink text="Contact Us" href={contactLink} target="_top"/>
         </Navigation>
       </div>
   )
