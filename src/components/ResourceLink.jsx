cd4b53b746b8540490832e66084e4afb926e61f1
Chris L <christoball@gmail.com>
2017-04-05 14:05:49 -0500

    playing with flood resources text

--- src/components/ResourceLink.jsx
+++ src/components/ResourceLink.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const ResourceLink = ({ text, href, target }) => {
+const ResourceLink = ({ text, href, title, target }) => {
   return (
     <a className="mdl-navigation__link resources__link" href={href} target={target}>
-      {text}
+      {text} &nbsp; <strong>{title}</strong>
     </a>
   )
 }
