4258507e8f4582471d7d5d8e071812359b10a39d
David Hickman <davidhickman13@gmail.com>
2017-04-19 11:13:08 -0500

    reverting url on popup close

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -44,6 +44,10 @@ export default class Popup extends Component {
         .on('popupclose', () => {
           this.removePopupContent()
           this.props.clearPopup()
+
+          const center = leafletMap.getCenter()
+          const zoom =  leafletMap.getZoom()
+          hashHistory.push(`/map/@${center.lat.toPrecision(7)},${center.lng.toPrecision(7)},${zoom}z`)
         })
         .on('popupopen', () => {
           this.updatePopupSize()
