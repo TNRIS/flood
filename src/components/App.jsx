f949ada54f25ce7f3137380426f950edf9ff11f4
adambreznicky <smudgeservices@gmail.com>
2017-04-19 15:24:08 -0500

    fixedDrawer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,7 +37,7 @@ export default class App extends Component {
         <Disclaimer />
         <SubscribeContainer />
         <AboutContainer />
-        <Layout fixedHeader>
+        <Layout fixedDrawer>
           <FloodHeaderContainer />
             <NavigationDrawer
               navContentInitState={navContentInitState()}
