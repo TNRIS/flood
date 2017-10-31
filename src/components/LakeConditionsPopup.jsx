33e973b93fb25f7a870758aee28b1a9e7627c432
adambreznicky <smudgeservices@gmail.com>
2017-10-31 13:48:32 -0500

    change lake image to https

--- src/components/LakeConditionsPopup.jsx
+++ src/components/LakeConditionsPopup.jsx
@@ -21,7 +21,7 @@ export default class LakeConditionsPopup extends Component {
 
   render() {
     const { full_name, lake_url_name, updatePopup } = this.props
-    const image_src = `http://waterdatafortexas.org/reservoirs/api/individual/${lake_url_name}/recent-elevations-chart`
+    const image_src = `https://waterdatafortexas.org/reservoirs/api/individual/${lake_url_name}/recent-elevations-chart`
     const link_src = `https://waterdatafortexas.org/reservoirs/individual/${lake_url_name}`
 
     return (
