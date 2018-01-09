f3b51fa708eb4ff5f910e8a9c282195f3182a4e9
Brez <smudgeservices@gmail.com>
2018-01-09 11:10:41 -0600

    disclaimer

--- src/components/Disclaimer.jsx
+++ src/components/Disclaimer.jsx
@@ -69,51 +69,50 @@ class Disclaimer extends React.Component {
     }
 
     return (
-      <div className="disclaimer__wrapper">
-        <Modal className="disclaimer-modal"
-               isOpen={this.state.openDialog}
-               contentLabel="Disclaimer Modal"
-               style={reactModalStyle}>
-          <div className="card disclaimer">
-            <div className="card-divider disclaimer-title">Legal Review and Disclaimer</div>
-            <div className="card-section disclaimer-text">
-              <p>
-              The data and information presented in this viewer is the best available
-              information provided to the Texas Water Development Board (TWDB) by its
-              data contributors. The information may not be represented in real-time
-              and should not be considered as exact conditions in your area. Neither
-              the State of Texas nor the Texas Water Development Board (TWDB) assumes
-              any legal liability or responsibility or makes any guarantees or warranties
-              as to the accuracy, completeness, or suitability of the information for
-              any purpose. By using this viewer or subscribing to a gage or gages, you agree
-              to release and hold harmless TWDB and its respective representatives, agents,
-              successors, assigns, employees, officers and directors, from any and all
-              liability, for loss, harm, damage, injury, cost or expense whatsoever
-              including without limitation, property damage, personal injury and/or death
-              which may occur in connection with the application service. If you have any
-              questions, please contact us at
-              <a href="https://tnris.org/contact/" target="_blank"> https://tnris.org/contact/</a>
-              </p>
-              {notice}
-            </div>
-            <div className="disclaimer-button-div">
-              <button className="button terms-agree-button" type="button" onClick={this.handleCloseDialog}>
-              I have read and agree to these terms
+      <Modal className="disclaimer-modal"
+             isOpen={this.state.openDialog}
+             contentLabel="Disclaimer Modal"
+             style={reactModalStyle}>
+        <div className="card disclaimer">
+          <h2 className="disclaimer-title">Legal Review and Disclaimer</h2>
+          <div className="card-section disclaimer-text">
+            <p>
+            The data and information presented in this viewer is the best available
+            information provided to the Texas Water Development Board (TWDB) by its
+            data contributors. The information may not be represented in real-time
+            and should not be considered as exact conditions in your area. Neither
+            the State of Texas nor the Texas Water Development Board (TWDB) assumes
+            any legal liability or responsibility or makes any guarantees or warranties
+            as to the accuracy, completeness, or suitability of the information for
+            any purpose. By using this viewer or subscribing to a gage or gages, you agree
+            to release and hold harmless TWDB and its respective representatives, agents,
+            successors, assigns, employees, officers and directors, from any and all
+            liability, for loss, harm, damage, injury, cost or expense whatsoever
+            including without limitation, property damage, personal injury and/or death
+            which may occur in connection with the application service. If you have any
+            questions, please contact us at
+            <a href="https://tnris.org/contact/" target="_blank"> https://tnris.org/contact/</a>
+            </p>
+            {notice}
+          </div>
+          <div className="disclaimer-button-div">
+            <button className="button terms-agree-button" type="button" onClick={this.handleCloseDialog}>
+            I have read and agree to these terms
             </button>
-              <div className="switch">
-                <input className="switch-input hide-disclaimer-checkbox"
-                  id="noShowSwitch"
-                  type="checkbox"
-                  name="noShowSwitch"
-                  onChange={updateDisclaimerAcceptance} />
-                <label className="switch-paddle" for="noShowSwitch">
-                  <span className="show-for-sr">Do not show again</span>
-                </label>
-              </div>
+            <div className="switch tiny">
+              <input className="switch-input hide-disclaimer-checkbox"
+                id="noShowSwitch"
+                type="checkbox"
+                name="noShowSwitch"
+                onChange={updateDisclaimerAcceptance} />
+              <label className="switch-paddle" for="noShowSwitch">
+                <span className="show-for-sr">Do not show again</span>
+              </label>
+              <span className="switch-label-text">Do not show again</span>
             </div>
           </div>
-        </Modal>
-      </div>
+        </div>
+      </Modal>
     )
   }
 }
