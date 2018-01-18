0b8cf97c97b22af39126c9531a24282aa96fc488
Brez <smudgeservices@gmail.com>
2018-01-18 15:59:44 -0600

    initial react alert as toaster replacement

--- src/components/Toaster.jsx
+++ src/components/Toaster.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ToastMessageAnimated } from "react-toastr";
+import Alert from 'react-s-alert'
 
 /**
  * App level toast notification component
@@ -18,18 +18,32 @@ class Toaster extends React.Component {
 
   constructor() {
     super()
+    console.log('toaster')
   }
 
+  componentDidUpdate () {
+    console.log(this.props)
+    // const colors = { background: '#4F85AE', text: "#FFFFFF" }
+    // notify.show(this.props.snackbarText, 'custom', 99999999, colors)
+    const options = {
+      position: 'bottom',
+      effect: 'genie',
+      onClose: this.props.hideSnackbar,
+      beep: false,
+      timeout: this.props.snackbarTimeout
+    }
+    Alert.info(this.props.snackbarText, options)
+  }
+  // {/* <ToastMessageAnimated
+  // showAnimation="bounceInUp"
+  // active={this.props.isSnackbarActive.toString()}
+  // {/* timeOut= */}
+  // timeOut=99999
+  // onRemove={this.props.hideSnackbar}
+  // message={this.props.snackbarText} /> */}
   render() {
     return (
-      <div>
-        <ToastMessageAnimated
-        showAnimation="bounceInUp"
-        active={this.props.isSnackbarActive.toString()}
-        timeOut={this.props.snackbarTimeout}
-        onRemove={this.props.hideSnackbar}
-        message={this.props.snackbarText} />
-      </div>
+      <Alert stack={{limit: 3}} />
     )
   }
 }
