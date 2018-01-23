e6888bb5060dff31f8dcce9221ccb405d0062345
Brez <smudgeservices@gmail.com>
2018-01-23 11:02:36 -0600

    react-router-dom

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
-// import { Router, Route, hashHistory } from 'react-router'
+// import { Router, Route, hashHistory } from 'react-router-dom'
 
 import AppContainer from './containers/AppContainer'
 
