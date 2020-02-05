642bae4f995272733032a4b93fb0a1b10b38784e
brez <adam.breznicky@twdb.texas.gov>
2020-02-05 11:22:16 -0600

    package updates. altered document event watching of foundation offcanvas menu for latest syntax (#198)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -60,12 +60,12 @@ export default class App extends Component {
 
   render() {
     // Hides and reveals the twitter widgets when the NavigationDrawer opens or closes
-    $(document).on("closed.zf.offcanvas", () => {
+    $(document).on("close.zf.offCanvas", () => {
       $("iframe").addClass("hidden-twitter")
       $(".weather-timestamp").removeClass("weather-timestamp-menu-open")
       $(".animate-radar").removeClass("animate-radar-menu-open")
     })
-    $(document).on("opened.zf.offcanvas", () => {
+    $(document).on("opened.zf.offCanvas", () => {
       $("iframe").removeClass("hidden-twitter")
       $(".weather-timestamp").addClass("weather-timestamp-menu-open")
       $(".animate-radar").addClass("animate-radar-menu-open")
